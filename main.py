5c605b2b1b3dcbe487613a8a5df3a9477957454f
Farouk Adeleke <fadeleke57@gmail.com>
2025-07-07 18:15:17 -0400

    rudimentary api

--- main.py
+++ main.py
@@ -1,20 +1,57 @@
 import os
 import tempfile
 from fastapi import FastAPI
-from markitdown import MarkItDown
-from pydantic import BaseModel  
+from fastapi.responses import JSONResponse
+from fastapi.exceptions import HTTPException
+import pymupdf4llm
+import pymupdf.pro
+import boto3
+from pydantic import BaseModel
+from config import settings
 
 app = FastAPI()
-md = MarkItDown()
+s3 = boto3.client("s3")
+pymupdf.pro.unlock()
+
 class ConvertToMarkdownPayload(BaseModel):
     file_key: str
 
-@app.post("/convert")
-def convert_to_markdown(payload: ConvertToMarkdownPayload):
-    tempdir = tempfile.TemporaryDirectory()
-    with open(os.path.join(tempdir.name, payload.file_key), "wb") as f:
-        f.write(payload.file_key)
+class BaseException(HTTPException):
+    def __init__(self, message: str, status_code: int = 500):
+        super().__init__(status_code=status_code, detail=message)
 
-    data = md.convert(os.path.join(tempdir.name, payload.file_key))
-    markdown = data.text_content
-    return {"markdown": markdown}
+class PyMuPDFProException(BaseException):
+    def __init__(self):
+        super().__init__(message="PyMuPDFPro exception", status_code=500)
+
+
+@app.post("/api/v1/convert")
+def convert_to_markdown(payload: ConvertToMarkdownPayload):
+    try:
+        path = os.path.join(tempfile.gettempdir(), payload.file_key)
+        s3.download_file(settings.s3_bucket_name, payload.file_key, path)
+
+        try:
+            data = pymupdf4llm.to_markdown(
+                doc=path,
+                page_chunks=True,
+            )
+            markdown = data
+        except Exception as e:
+            raise PyMuPDFProException(message=str(e))
+        finally:
+            os.remove(path)
+
+        return JSONResponse(content={"markdown": markdown})
+    except Exception as e:
+        raise BaseException(message=str(e))
+
+
+@app.get("/health")
+def health():
+    return JSONResponse(content={"status": "ok"})
+
+
+@app.get("/")
+def root():
+    return JSONResponse(content={"status": "ok"})
