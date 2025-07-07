1f0f9f5b1899dd61d67a7005968ae0d6dcd35751
Farouk Adeleke <fadeleke57@gmail.com>
2025-07-07 15:25:22 -0400

    minimal fastapi app

---
+++ main.py
@@ -0,0 +1,22 @@
+import os
+import tempfile
+from fastapi import FastAPI
+from markitdown import MarkItDown
+from pypdf import PdfReader
+from pydantic import BaseModel
+from boto3 import client as s3_client
+
+app = FastAPI()
+md = MarkItDown()
+class ConvertToMarkdownPayload(BaseModel):
+    file_key: str
+
+@app.post("/convert")
+def convert_to_markdown(payload: ConvertToMarkdownPayload):
+    tempdir = tempfile.TemporaryDirectory()
+    with open(os.path.join(tempdir.name, payload.file_key), "wb") as f:
+        s3_client.download_file("", payload.file_key, f)
+
+    data = md.convert(os.path.join(tempdir.name, payload.file_key))
+    markdown = data.text_content
+    return {"markdown": markdown}
