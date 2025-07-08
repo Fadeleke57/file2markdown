ca89e6df2ac0f52cfa13f6133ad73851789283b7
Farouk Adeleke <fadeleke57@gmail.com>
2025-07-07 22:17:09 -0400

    markdown service

--- main.py
+++ main.py
@@ -7,12 +7,37 @@ import boto3
 from pydantic import BaseModel
 from config import settings
 from extraction import service as extraction_service
+from fastapi.middleware.cors import CORSMiddleware
+from logger import logger
+import os
+
 
 app = FastAPI()
 s3 = boto3.client("s3")
 
+origins = [
+    "http://localhost:3000",
+    "http://localhost:8000",
+    "https://spyderweb.vercel.app",
+    "https://www.spydr.dev",
+    "https://spydr.dev",
+    "https://api.spydr.dev",
+    "https://vercel.spydr.dev",
+    "https://spydrweb-git-feature-farouk-adelekes-projects.vercel.app",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
 class ConvertToMarkdownPayload(BaseModel):
     file_key: str
+
 
 class BaseException(HTTPException):
     def __init__(self, message: str, status_code: int = 500):
@@ -24,21 +49,40 @@ class PyMuPDFProException(BaseException):
         super().__init__(message="PyMuPDFPro exception", status_code=500)
 
 
+class FileDownloadException(BaseException):
+    def __init__(self, message: str):
+        super().__init__(message=f"File download exception: {message}", status_code=500)
+
+
 @app.post("/api/v1/convert")
 def convert_to_markdown(payload: ConvertToMarkdownPayload):
     try:
-        path = os.path.join(tempfile.gettempdir(), payload.file_key)
-        s3.download_file(settings.s3_bucket_name, payload.file_key, path)
+        filename = os.path.basename(payload.file_key)
+        path = os.path.join(tempfile.gettempdir(), filename)
+        
+        logger.error(f"FILE PATH: {path}")
+        logger.error(f"FILE KEY: {payload.file_key}")
 
         try:
-            markdown = extraction_service.extract(path)
-            return JSONResponse(content={"markdown": markdown})
+            s3.download_file(settings.s3_bucket_name, payload.file_key, path)
         except Exception as e:
-            raise PyMuPDFProException(message=str(e))
+            logger.error(f"FILE DOWNLOAD FAILED: {str(e)}")
+            raise FileDownloadException(message=str(e))
+        
+        logger.error(f"FILE DOWNLOADED: {path}")
+        try:
+            data = extraction_service.extract(path)
+            logger.error(f"EXTRACTION SERVICE SUCCESS: {str(data)}")
+            return JSONResponse(content={"result": data})
+        except Exception as e:
+            logger.error(f"EXTRACTION SERVICE FAILED: {str(e)}")
+            raise PyMuPDFProException()
         finally:
+            logger.error(f"FILE REMOVED: {path}")
             os.remove(path)
 
     except Exception as e:
+        logger.error(f"CONVERT TO MARKDOWN FAILED: {str(e)}")
         raise BaseException(message=str(e))
 
 
